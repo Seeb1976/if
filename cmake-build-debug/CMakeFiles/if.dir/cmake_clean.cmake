d48734bae164808a266131c924dd4aadfe2100e7
seeb1976 <spuzdrowski@pomorska.policja.gov.pl>
2018-10-12 17:49:14 +0200

    delta

---
+++ cmake-build-debug/CMakeFiles/if.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/if.dir/if.c.o"
+  "if.pdb"
+  "if"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/if.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
